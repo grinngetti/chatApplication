589785ffff3c0abaf4701173b5016e7c56d3367e
guljamal <guljamal.usubakunova@gmail.com>
2021-11-16 22:27:30 +0600

    init project

---
+++ chatApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace chatApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
